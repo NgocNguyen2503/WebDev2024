c48f81be2c56ca0a3100c44a470fd32a0f015a17
NgocNguyen <quangquangngoc@gmail.com>
2024-10-04 21:00:30 +0700

    Fix the login screen and gitignore README

--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './login.css'
 import large_logo from '../../assets/logo_icon.png'
+import appstore from '../../assets/app_store.png'
+import microsoft from '../../assets/microsoft.png'
 import Login_Navbar from '../../components/loginNavBar/Login_Navbar'
 
 const Login = () => {
@@ -14,9 +16,20 @@ const Login = () => {
                         <p>anytime,</p>
                         <p>anywhere</p>
                     </div>
-                    <div className="login-description"></div>
-                    <div className="login-form"></div>
-                    <div className="login-platform"></div>
+                    <div className="login-description">
+                        <p>Stay in Touch with Special People and</p>
+                        <p>Make New Friends!</p>
+                    </div>
+                    <div className="login-container">
+                        <input type="email" placeholder='Email address' />
+                        <input type="password" placeholder='Password' />
+                        <button>Login</button>
+                    </div>
+                    <div className="login-platform">
+                        <img className="microsoft" src={microsoft} alt="Microsoft" />
+                        <img className="appstore" src={appstore} alt="app store" />
+
+                    </div>
                 </div>
                 <div className="right-login">
                     <img src={large_logo} alt='large logo' />
