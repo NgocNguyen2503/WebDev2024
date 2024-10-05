0db8a4bc0db680261c7ccb2de9b394f993542d76
NgocNguyen <quangquangngoc@gmail.com>
2024-10-05 11:54:38 +0700

    Set the router, Create 3 pages in navbar, Create Signup page

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login/Login'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
+import Login from './pages/Login/Login'
+import Sign_up from './pages/Sign_up/Sign_up'
+import Login_Navbar from './components/loginNavBar/Login_Navbar'
+import Features from './pages/Features/Features'
+import DesktopApp from './pages/DesktopApp/DesktopApp'
+import Privacy from './pages/Privacy/Privacy'
+
+
 
 const App = () => {
+  const location = useLocation()
+  const showNavbar = location.pathname !== '/chat';
   return (
     <>
+      {showNavbar ? <Login_Navbar /> : <></>}
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Sign_up />} />
+        <Route path='/features' element={<Features />} />
+        <Route path='/desktop-app' element={<DesktopApp />} />
+        <Route path='/privacy' element={<Privacy />} />
         <Route path='/chat' element={<Chat />} />
         <Route path='/profile' element={<ProfileUpdate />} />
       </Routes>
