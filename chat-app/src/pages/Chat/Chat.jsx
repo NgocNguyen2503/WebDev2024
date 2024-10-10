a0faada92236876f0b31ccb114dee3bb721a66d5
nghia nguyen <nghia@gmail.com>
2024-10-10 23:23:13 +0700

    merge chat

--- chat-app/src/pages/Chat/Chat.jsx
+++ chat-app/src/pages/Chat/Chat.jsx
@@ -5,11 +5,20 @@ import Chat_group from '../../components/Group_chat/Chat_group'
 import InfoSection from '../../components/InfoSection/InfoSection'
 const Chat = () => {
     return (
-        <div className='chat'>
-            <MainLayout />
-            <Chat_group />
-            <InfoSection />
+     
+    <div class="main-layout">
+        <div class="left">
+         <MainLayout />
         </div>
+        
+        <div class="mid">
+          <Chat_group />
+        </div>
+        
+        <div class="right">
+          <InfoSection />
+        </div>
+    </div>
     )
 }
 
